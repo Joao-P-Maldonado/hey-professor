7b2534f312acd0e573d4011812c044895d0a243c
Maldonado <joaopedromaldonado03@gmail.com>
2024-07-13 21:59:51 -0300

    HEY-2: teste

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -15,7 +15,9 @@ class ProfileController extends Controller
     public function edit(Request $request): View
     {
         return view('profile.edit', [
+
             'user' => $request->user(),
+
         ]);
     }
 
